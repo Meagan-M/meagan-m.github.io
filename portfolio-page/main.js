f3025910013389643748bad76462dcf5d6958c78
Meagan-M <meaganmakinson80@gmail.com>
2019-08-21 16:12:29 -0400

    style projects and about me section, add buttons for scrollintoview feature with basic styling, begin writing js function for scrolling.

---
+++ portfolio-page/main.js
@@ -0,0 +1,17 @@
+// Auto scrolls to a new section when the link is clicked
+function scrollTo() {
+    var elem = document.getElementById("projects");
+    elem.scrollIntoView();
+}
+
+scrollTo(projects);
+
+
+function scrollTo() {
+    var elem = document.getElementById("resume");
+    elem.scrollIntoView();
+}
+
+scrollTo(resume);
+
+
